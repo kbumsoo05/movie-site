39ae339c33fe8e0e1233cd8271148c390dd15315
Kim Bumsu <53156566+kbumsoo05@users.noreply.github.com>
2023-02-11 14:45:28 +0000

    fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
 import React, { useState, useRef, useEffect } from "react";
 
 const App = () => {
-  const [toDo, setToDo] = React.useState("");
-  const [toDos, setToDos] = React.useState([]);
-  const onChange = (event) => { setToDo(event.target.value) };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((prev) => [toDo, ...prev]);
-    setToDo("");
-  };
-  const onClick = (event) => { 
-    console.log(event.nativeEvent.path);
-    event.nativeEvent.path[2].removeChild(event.nativeEvent.path[1]);
-  };
-  
+  const [load, setLoad] = React.useState(true);
+  const [item, setItem] = React.useState([]);
+
+  useEffect(() => {
+    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
+      .then((respones) => respones.json())
+      .then((json) => {
+        setItem(json.data.movies);
+        setLoad(false);
+        console.dir(json.data.movies);
+        console.dir(item);
+      });
+  }, [])
+
   return (
     <div>
-      <h1>To Do List ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-        />
-        <button>add</button>
-      </form>
-      <hr />
+      <h1>Movie</h1>
+      {load ? <h5>loading...</h5> : null}
       <ul>
-        {toDos.map((item, index) => <li key={index}>{item} <button onClick={onClick}>X</button></li>)}
+        {item.map((item) => <li>{item.title}</li>)}
       </ul>
     </div>
   );
